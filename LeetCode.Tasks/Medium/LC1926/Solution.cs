b068bde4d5b681b2f2b896db5bdf680fe2f5fe2d
Roman Matveev <roman89203323606@gmail.com>
2024-11-03 22:15:42 +0300

    Solve Medium 1926

---
+++ LeetCode.Tasks/Medium/LC1926/Solution.cs
@@ -0,0 +1,89 @@
+﻿// 1926. Nearest Exit from Entrance in Maze
+// https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/description
+
+// You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+'). You are also given the entrance of the maze, where entrance = [entrancerow, entrancecol] denotes the row and column of the cell you are initially standing at.
+// In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall, and you cannot step outside the maze. Your goal is to find the nearest exit from the entrance. An exit is defined as an empty cell that is at the border of the maze. The entrance does not count as an exit.
+// Return the number of steps in the shortest path from the entrance to the nearest exit, or -1 if no such path exists.
+
+
+namespace LeetCode.Tasks.Medium.LC1926;
+
+public class Solution
+{
+    public int NearestExit(char[][] maze, int[] entrance)
+    {
+        const char empty = '.';
+        const char visited = '!';
+        int m = maze.Length - 1;
+        int n = maze[0].Length - 1;
+        int x = entrance[0];
+        int y = entrance[1];
+
+        Queue<MazePoint> queue = new();
+
+        maze[x][y] = visited;
+        int offset = x - 1;
+        if (offset >= 0 && offset <= m && maze[offset][y] == empty)
+        {
+            maze[offset][y] = visited;
+            queue.Enqueue(new() { X = offset, Y = y, Value = 1 });
+        }
+        offset = x + 1;
+        if (offset >= 0 && offset <= m && maze[offset][y] == empty)
+        {
+            maze[offset][y] = visited;
+            queue.Enqueue(new() { X = offset, Y = y, Value = 1 });
+        }
+        offset = y - 1;
+        if (offset >= 0 && offset <= n && maze[x][offset] == empty)
+        {
+            maze[x][offset] = visited;
+            queue.Enqueue(new() { X = x, Y = offset, Value = 1 });
+        }
+        offset = y + 1;
+        if (offset >= 0 && offset <= n && maze[x][offset] == empty)
+        {
+            maze[x][offset] = visited;
+            queue.Enqueue(new() { X = x, Y = offset, Value = 1 });
+        }
+
+        while (queue.TryDequeue(out MazePoint point))
+        {
+            if (point.X == 0 || point.X == m || point.Y == 0 || point.Y == n)
+                return point.Value;
+
+            int val = point.Value + 1;
+            if (maze[point.X - 1][point.Y] == empty)
+            {
+                maze[point.X - 1][point.Y] = visited;
+                queue.Enqueue(new() { X = point.X - 1, Y = point.Y, Value = val });
+            }
+
+            if (maze[point.X + 1][point.Y] == empty)
+            {
+                maze[point.X + 1][point.Y] = visited;
+                queue.Enqueue(new() { X = point.X + 1, Y = point.Y, Value = val });
+            }
+
+            if (maze[point.X][point.Y - 1] == empty)
+            {
+                maze[point.X][point.Y - 1] = visited;
+                queue.Enqueue(new() { X = point.X, Y = point.Y - 1, Value = val });
+            }
+
+            if (maze[point.X][point.Y + 1] == empty)
+            {
+                maze[point.X][point.Y + 1] = visited;
+                queue.Enqueue(new() { X = point.X, Y = point.Y + 1, Value = val });
+            }
+        }
+        return -1;
+    }
+
+    private struct MazePoint
+    {
+        public int X;
+        public int Y;
+        public int Value;
+    }
+}
---
+++ LeetCode.Tests/Medium/LC1926/Tests.cs
@@ -0,0 +1,68 @@
+﻿using LeetCode.Tasks.Medium.LC1926;
+
+namespace LeetCode.Tests.Medium.LC1926;
+
+public class Tests
+{
+    [Fact]
+    public void Test_1()
+    {
+        int nearest = new Solution().NearestExit(
+            [
+                ['+', '+', '.', '+'], 
+                ['.', '.', '.', '+'], 
+                ['+', '+', '+', '.']
+            ], [1, 2]);
+        Assert.Equal(1, nearest);
+    }
+
+    [Fact]
+    public void Test_2()
+    {
+        int nearest = new Solution().NearestExit(
+            [
+                ['+', '+', '+'], 
+                ['.', '.', '.'], 
+                ['+', '+', '+']
+            ], [1, 0]);
+        Assert.Equal(2, nearest);
+    }
+
+    [Fact]
+    public void Test_3()
+    {
+        int nearest = new Solution().NearestExit(
+            [
+                ['.', '+']
+            ], [0, 0]);
+        Assert.Equal(-1, nearest);
+    }
+
+    [Fact]
+    public void Test_4()
+    {
+        int nearest = new Solution().NearestExit(
+            [
+                ['+', '.', '+', '+', '+', '+', '+'], 
+                ['+', '.', '+', '.', '.', '.', '+'], 
+                ['+', '.', '+', '.', '+', '.', '+'], 
+                ['+', '.', '.', '.', '+', '.', '+'], 
+                ['+', '+', '+', '+', '+', '.', '+']
+            ], [0, 1]);
+        Assert.Equal(12, nearest);
+    }
+
+    [Fact]
+    public void Test_5()
+    {
+        int nearest = new Solution().NearestExit(
+            [
+                ['+', '.', '+', '+', '+', '+', '+'], 
+                ['+', '.', '+', '.', '.', '.', '+'], 
+                ['+', '.', '+', '.', '+', '.', '+'], 
+                ['+', '.', '.', '.', '.', '.', '+'], 
+                ['+', '+', '+', '+', '.', '+', '.']
+            ], [0, 1]);
+        Assert.Equal(7, nearest);
+    }
+}
