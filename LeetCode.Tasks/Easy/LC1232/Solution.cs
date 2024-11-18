3aa217101f2519606c470ab9527f084c8cd84849
Roman Matveev <roman89203323606@gmail.com>
2024-11-18 18:02:21 +0300

    Solve Easy 1232

---
+++ LeetCode.Tasks/Easy/LC1232/Solution.cs
@@ -0,0 +1,24 @@
+﻿// 1232.Check If It Is a Straight Line
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/description/
+
+// You are given an array coordinates, coordinates[i] = [x, y], where[x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+namespace LeetCode.Tasks.Easy.LC1232;
+
+public class Solution
+{
+    public bool CheckStraightLine(int[][] coordinates)
+    {
+        if (coordinates.Length == 2)
+            return true;
+        var (x1, y1) = (coordinates[0][0], coordinates[0][1]);
+        var (x2, y2) = (coordinates[1][0], coordinates[1][1]);
+        for (int i = 2; i < coordinates.Length; i++)
+        {
+            var (x, y) = (coordinates[i][0], coordinates[i][1]);
+            if ((x - x1) * (y2 - y1) != (y - y1) * (x2 - x1))
+                return false;
+        }
+        return true;
+    }
+}
---
+++ LeetCode.Tests/Easy/LC1232/Tests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Tasks.Easy.LC1232;
+
+namespace LeetCode.Tests.Easy.LC1232;
+
+public class Tests
+{
+    [Fact]
+    public void Test_1()
+    {
+        bool result = new Solution().CheckStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]);
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Test_2()
+    {
+        bool result = new Solution().CheckStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]);
+        Assert.False(result);
+    }
+}
